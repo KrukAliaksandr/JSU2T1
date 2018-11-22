ca7942fd4999c604d9ab135f52b52e3f205f94f6
Aliaksandr Kruk <Aliaksandr_Kruk@epam.com>
2018-11-22 18:32:38 +0300

    Function logic edits

--- task1.js
+++ task1.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
-const wow = require("yargs");
+const todo = require("yargs");
 const fs = require("fs");
 const path = require("path");
 
-wow.command(
-	"todo",
-	"makes and action with a file",
+todo.command(
+	"Add",
+	"makes an action with a file",
 	function (yargs) {
 		return yargs.options({
 			"path": {
@@ -13,66 +13,151 @@ wow.command(
 				describe: "path to file",
 				demandOption: true
 			},
-			"action": {
-				alias: "a",
-				describe: "action for file",
-				demandOption: true
-			},
 			"title": {
 				alias: "t",
 				describe: "node title",
+				demandOption: true
 			},
 			"body": {
 				alias: "b",
 				describe: "node body",
+				demandOption: true
 			}
 		});
 	},
 
 	function (argv) {
-		switch (argv.action) {
-		case "Add": checkArgs(argv);
-			addNodeToFile(argv);
-			break;
-		case "List": checkArgs(argv);
-			listAllNodes(argv);
-			break;
-		case "Read": checkArgs(argv);
-			readNodeFromFile(argv);
-			break;
-		case "Remove": checkArgs(argv);
-			removeNodeFromFile(argv);
-			break;
-		}
+		checkArgs(argv);
+		addNoteToFile(argv);
+
 	}
 )
 	.help()
 	.argv;
 
-function addNodeToFile(argv) {
-	// eslint-disable-next-line no-console
-	fs.writeFile(argv.path + ".json", JSON.stringify({ title: argv.title, body: argv.body }), "utf8", () => {
-		// eslint-disable-next-line no-console
-		console.log(argv.path + "successfully written");
-	});
+todo.command(
+	"List",
+	"makes an action with a file",
+	function (yargs) {
+		return yargs.options({
+			"path": {
+				alias: "p",
+				describe: "path to file",
+				demandOption: true
+			}
+		});
+	},
+
+	function (argv) {
+		checkArgs(argv);
+		listAllNotes(argv);
+	}
+)
+	.help()
+	.argv;
+
+todo.command(
+	"Read",
+	"makes an action with a file",
+	function (yargs) {
+		return yargs.options({
+			"path": {
+				alias: "p",
+				describe: "path to file",
+				demandOption: true
+			},
+			"title": {
+				alias: "t",
+				describe: "node title",
+				demandOption: true
+			}
+		});
+	},
+
+	function (argv) {
+		checkArgs(argv);
+		readNoteFromFile(argv);
+	}
+)
+	.help()
+	.argv;
+
+todo.command(
+	"Remove",
+	"makes an action with a file",
+	function (yargs) {
+		return yargs.options({
+			"path": {
+				alias: "p",
+				describe: "path to file",
+				demandOption: true
+			},
+			"title": {
+				alias: "t",
+				describe: "node title",
+				demandOption: true
+			}
+		});
+	},
+
+	function (argv) {
+		checkArgs(argv);
+		removeNoteFromFile(argv);
+	}
+)
+	.help()
+	.argv;
+
+function addNoteToFile(argv) {
+	// eslint-disable-next-line no-console]
+	const jsonObject = fs.readFileSync(argv.path + ".json","utf8");
+	console.log(jsonObject);
+	const fileStrings = jsonObject.split("\r\n");
+	console.log(fileStrings.length);
+	if (fileStrings.length === 1 && fileStrings[0].match(/{(.*)}/g)=== null) {
+		fs.writeFile(argv.path + ".json", JSON.stringify([{ title: argv.title, body: argv.body }],null,"\t"), "utf8", () => {
+			console.log(argv.path + " successfully written");
+		});
+	} 
+	// else if(fileStrings[0].match(/{(.*)}/g)!== null && fileStrings[fileStrings.length-1].match(/{(.*)}/g)!== null ||
+	// fileStrings[0].match(/{.*/g)!== null && fileStrings[fileStrings.length-1].match(/.*}/g)!== null ){
+	// 	fileStrings.push("]");
+	// 	fileStrings.unshift("[");
+	// 	fs.writeFile(argv.path + ".json", JSON.stringify([{fileStrings}],null,"\t"), "utf8", () => {
+	// 		console.log(argv.path + " successfully corrected");
+	// 	});
+	// }
+	if (jsonObject.match(/(\[\n?((\t)|( ))*(((\t)|( ))*{\n((\t)|( ))*"[a-z]*": "[a-z]*",\n((\t)|( ))*"[a-z]*": "[a-z]*"\n((\t)|( ))*},*\n*)*\n\]?)/g)) {
+		let jsonStrings = JSON.parse(jsonObject);
+		console.log(jsonObject);
+		console.log(jsonStrings);
+		jsonStrings.push({ title: argv.title, body: argv.body });
+		console.log(jsonStrings);
+		fs.writeFile(argv.path + ".json", JSON.stringify(jsonStrings,null,"\t"), "utf8", () => {
+			// eslint-disable-next-line no-console
+			console.log(argv.path + "successfully written");
+		});
+	}
+
 }
 
-function listAllNodes(argv) {
+function listAllNotes(argv) {
 	const jsonObject = require("./" + argv.path + ".json");
 	jsonObject.forEach(function (node) {
 		console.log(node);
 	});
 }
 
-function readNodeFromFile(argv) {
+function readNoteFromFile(argv) {
 	const jsonObject = require("./" + argv.path + ".json");
+	console.log(jsonObject.length);
 	const result = jsonObject.filter(function (node) {
 		return node.title === argv.title;
 	});
 	console.log(result);
 }
 
-function removeNodeFromFile(argv) {
+function removeNoteFromFile(argv) {
 	const jsonObject = require("./" + argv.path + ".json");
 	const result = jsonObject.forEach(function (node) {
 		if (node.title === argv.title) delete jsonObject[node];
@@ -85,10 +170,10 @@ function removeNodeFromFile(argv) {
 
 function checkArgs(argv) {
 	if (argv.action === "Add") {
-		if (argv.title === undefined || argv.body === undefined) {
+		if (typeof argv.title === "undefined" || typeof argv.body === "undefined") {
 			throw new Error();
 		}
-		if ((argv.action === "Read" || argv.action === "Remove") && argv.title === undefined) {
+		if ((argv.action === "Read" || argv.action === "Remove") && typeof argv.title === "undefined") {
 			throw new Error();
 		}
 	}
